6c887e74ed6b65e99d23c51de8950a8c21534138
方太鸿\ST Cloud QA Part <fang_taihong@nhn-st.com>
2020-08-05 16:00:54 +0900

    Revert "[Redis] 파일 생성 및 Redis 접속시 패스워드 사용 여부 추가"
    This reverts commit 1eb82571351c0fce9cfed6b6f28ef88952e071d1.

--- redis_set_data.js
+++ redis_set_data.js
@@ -1,4 +1,4 @@
-// redis 연결하여 key iter번 조회
+// redis 연결하여 10M value인 key iter개 생성
 
 const args = process.argv;
 const domain = args[2];
@@ -9,22 +9,20 @@ const iter = args[6];
 
 var redis = require('redis');
 var shell = require('shelljs');
+var randomString = require('random-string');
 
 var client = redis.createClient({host: domain, port: port}); 
-if (null != pw) {
-	client.auth(pw); 
-}
+client.auth(pw); 
 client.on('error', err => console.log('------ Redis connection failed ------' + err))
 	  .on('error', err => shell.exit(1)) 
 	  .on('connect', () => console.log('------ Redis connection succeed ------')); 
 
+var num = 10 * 1024 * 1024;
+var valueData = randomString({ length: num, numeric: false });
+
 for (var i = 0; i < iter; i++) {
-	client.get(key, function (err, data) { 
-		if (data == null) {
-			console.log('Get 결과: 존재하지 않는 Key');
-		} else {
-			console.log('Get 결과:', err);
-		}
-	}) 
+	client.set(key + i, valueData, function (err, result) {
+		console.log('Set 결과:', err, result);
+	});
 }
 client.quit();
